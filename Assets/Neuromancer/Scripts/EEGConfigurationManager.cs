a8036e8a367242f4a5c23a8a066e6a819298932c
Half0S <oliwiajankowska.contact@gmail.com>
2025-05-04 07:04:14 +0200

    Final repo fix

--- Assets/Neuromancer/Scripts/EEGConfigurationManager.cs
+++ Assets/Neuromancer/Scripts/EEGConfigurationManager.cs
@@ -1,22 +1,116 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using Gtec.Chain.Common.SignalProcessingPipelines;
+using Gtec.Chain.Common.Templates.Utilities;
 using Gtec.UnityInterface;
 using UnityEngine;
+using UnityEngine.UI;
+using static Gtec.Chain.Common.Nodes.InputNodes.ChannelQuality;
 
 public class EEGConfigurationManager : MonoBehaviour
 {
-    public GameObject ConfigurationUI;
-    private ERPParadigm _erpParadigm;
+    // Start configuration with test of 1.
+    // IF passed, test all 5 for 30 seconds.
+    // After the time, load the beginning of the game.
+
+    public bool CheckForTest1 = true;
+    public float WaitTimeTest2 = 30f;
+
+    public GameObject ConfigurationText1;
+    public GameObject ConfigurationText2;
+    public ERPParadigm _erpParadigm;
+    public Gtec.UnityInterface.ERPPipeline _erpPipeline;
+    public GameObject GameFlowManager;
+    public GameObject BciConfiguration;
+    public GameObject BciGameplay;
+
+    public List<GameObject> SpinningNeurons = new List<GameObject>();
+
+    //The Brain
+    public UnityEngine.Color ChannelGood;
+    public UnityEngine.Color ChannelBad;
+    public SignalQualityPipeline _signalQualityPipeline;
+    public List<Image> Images = new List<Image>();
 
     private void Awake()
     {
         StartConfiguration();
+        _erpPipeline.OnCalibrationResult.AddListener(OnClassifierAvailable);
+        _signalQualityPipeline.OnSignalQualityAvailable.AddListener(OnSignalQualityAvailable);
     }
 
     private void StartConfiguration()
     {
-        ConfigurationUI.SetActive(true);
-        Debug.Log("Im here!");
-        _erpParadigm.StartParadigm(ParadigmMode.Training);
+        ConfigurationText1.SetActive(true);
+        Invoke("StartParadigmTraining", 5f);
+    }
+
+    private void StartParadigmTraining()
+    {
+        _erpParadigm.StartParadigm(ParadigmMode.Training); //ParadigmMode.Training for one
+    }
+
+    private void StartParadigmApplication()
+    {
+        ConfigurationText1.SetActive(false);
+        ConfigurationText2.SetActive(true);
+        foreach (GameObject go in SpinningNeurons)
+        { 
+            go.SetActive(true); 
+        }
+        _erpParadigm.StartParadigm(ParadigmMode.Application); //ParadigmMode.Application for five
+        Invoke("StartGameFlow", WaitTimeTest2);
+    }
+
+    private void OnClassifierAvailable(ERPParadigm paradigm, CalibrationResult result)
+    {
+        EventHandler.Instance.Enqueue(() =>
+        {
+            if (result != null && paradigm != null)
+            {
+                if (result.CalibrationQuality == CalibrationQuality.Good || result.CalibrationQuality == CalibrationQuality.Ok)
+                {
+                    //Continue to Test 2
+                    StartParadigmApplication();
+                }
+                else if (result.CalibrationQuality == CalibrationQuality.Bad)
+                {
+                    if (CheckForTest1)
+                    {
+                        //Redo Test 1
+                        StartParadigmTraining();
+                    }
+                    else
+                    {
+                        StartParadigmApplication();
+                    }
+                }
+            }
+            else
+            {
+                //Null?
+            }
+        });
+    }
+
+    private void StartGameFlow()
+    {
+        _erpParadigm.StopParadigm();
+        GameFlowManager.SetActive(true);
+        BciConfiguration.SetActive(false);
+        BciGameplay.SetActive(true);
+    }
+
+    private void OnSignalQualityAvailable(List<ChannelStates> signalQuality)
+    {
+        for (int i = 0; i < signalQuality.Count; i++)
+        {
+            if (signalQuality[i].Equals(ChannelStates.Good))
+                Images[i].color = ChannelGood;
+            else
+                Images[i].color = ChannelBad;
+            Debug.Log("Channel " + i + ": " + signalQuality[i].ToString());
+        }
     }
 }
--- Assets/Neuromancer/Scripts/EEGManager.cs
+++ Assets/Neuromancer/Scripts/EEGManager.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using Gtec.UnityInterface;
 using UnityEngine;
 using UnityEngine.Events;
-using static Gtec.Chain.Common.Nodes.InputNodes.ChannelQuality;
 
 public class EEGManager : MonoBehaviour
 {
     public static EEGManager Instance { get; private set; }
-    private string serialName = "UN-0000.00.00"; //EEG Headset name is UN-2023.03.17, for testing can use UN-0000.00.00
+    private string serialName = "UN-2023.03.17"; //EEG Headset name is UN-2023.03.17, for testing can use UN-0000.00.00
     [SerializeField] private Device BciDevice;
     public UnityEvent OnConnect;
 
+    public bool IsSimulating;
     public bool IsVisual;
     public bool IsMotor;
     public bool IsAlpha;
@@ -30,7 +30,9 @@ public class EEGManager : MonoBehaviour
 
     private void Start()
     {
-        ConnectToDevice();
+        if (IsSimulating)
+            serialName = "UN-0000.00.00";
+        Invoke("ConnectToDevice", 5f);
     }
 
     public void ConnectToDevice()
@@ -39,23 +41,4 @@ public class EEGManager : MonoBehaviour
 
         OnConnect.Invoke();
     }
-
-    #region Signal Quality Pipeline Events
-    public void OnSignalQualityAvailable(List<ChannelStates> channelStates)
-    {
-        string channelStatesString = string.Empty;
-        for (int i = 0; i < channelStates.Count; i++)
-            channelStatesString += string.Format("{0}: {1}, ", i + 1, channelStates[i].ToString());
-        Debug.Log(channelStatesString);
-    }
-
-    public void OnDataAvailable(float[,] e)
-    {
-        string channelDataString = string.Empty;
-        for (int row = 0; row < e.GetLength(0); row++)
-            for (int col = 0; col < e.GetLength(1); col++)
-                channelDataString += string.Format("Frame:{0}, EEG Channel:{1}, Value:{2}, ", row + 1, col + 1, e[row, col].ToString());
-        Debug.Log(channelDataString);
-    }
-    #endregion
 }
